f3d16dce24d2d714761a8285897fbad72c2e7614
Lubna <muspaksamsu@gmail.com>
2024-07-01 22:36:16 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+//Adding Comments: Choose two of the programs you’ve written, 
+//and add at least one comment to each. If you don’t have anything specific to write 
+//because your programs are too simple at this point, just add your name and the current date at the top of 
+//each program file. Then write one sentence describing what the program does.
+
+//Author:{Lubna Riaz}
+//date:{tuesday,february 27,2024}
+//task 9:printing my favorite number.
+let favoriteNumber: number = 4;
+//revising my favorite number in a message format.
+console.log(`My favorite number is ${favoriteNumber}.`);
